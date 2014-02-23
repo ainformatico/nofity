e2941f122779c13ba4ec86208bd4318ea8ff7b8f
Alejandro El Informatico <aeinformatico@gmail.com>
2014-02-23 15:55:12 +0100

    ADD rails base version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nfy::Application.config.secret_key_base = '6cb3dd64dc4f45e6627efdc245a52ab70f1f63107a4b11378a7f7966bebf71f0653bd3ad74f7b176c0f357bd6ea090e20d2970d8d87954645624e77255624f4e'
